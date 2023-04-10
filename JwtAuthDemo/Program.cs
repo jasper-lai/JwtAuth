24e7c4f3e8360620897cec4980cf9e7de16f6532
jasper.lai <jasper.lai@gmail.com>
2023-04-10 14:18:31 +0800

    Add JwtHelpers to DI; Add LoginViewModel

--- JwtAuthDemo/Program.cs
+++ JwtAuthDemo/Program.cs
@@ -1,3 +1,5 @@
+using JwtAuthDemo.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region (jasper) Add JwtHelpers to DI
+builder.Services.AddSingleton<JwtHelpers>();
+#endregion
 
 var app = builder.Build();
 
---
+++ JwtAuthDemo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtAuthDemo.ViewModels
+{
+    public record LoginViewModel
+    {
+        public string UserName { get; init; }
+        public string Password { get; init; }
+
+        public LoginViewModel(string username, string password) => 
+            (UserName, Password) = (username, password);
+    }
+}
